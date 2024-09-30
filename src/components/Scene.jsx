d3c5fc9ff225c3cf286439a3d4b40fef54ad8993
Shiyi <scheng1@upv.edu.es>
2024-09-30 03:16:25 +0200

    Diseño de subpaginas y ajustes de ventanas

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -3,21 +3,23 @@ import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import Earth from './Earth';
-import { Preload } from "@react-three/drei"
+import { Preload } from "@react-three/drei";
 import CanvasLoader from "./Loader";
 import MovingStars from "../components/MovingStars.jsx";
 
 export default function Scene() {
     return (
-        <Canvas camera={{ position: [0, 0, 4] }} style={{ background: 'transparent' }}>
-            <Suspense fallback={<CanvasLoader />}>
-                <ambientLight intensity={0.5} />
-                <pointLight position={[10, 10, 10]} />
-                <MovingStars />
-                <Earth />
-                <OrbitControls enableZoom={false} />
-            </Suspense>
-            <Preload all />
-        </Canvas>
+        <div className="h-full max-h-[calc(100vh-<altura-del-navbar>)] w-full">
+            <Canvas camera={{ position: [0, 0, 4] }} style={{ background: 'transparent' }} className="h-full w-full">
+                <Suspense fallback={<CanvasLoader />}>
+                    <ambientLight intensity={0.5} />
+                    <pointLight position={[10, 10, 10]} />
+                    <MovingStars />
+                    <Earth />
+                    <OrbitControls enableZoom={false} />
+                </Suspense>
+                <Preload all />
+            </Canvas>
+        </div>
     );
 }
