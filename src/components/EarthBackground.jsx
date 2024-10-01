61d73457ca21a77113d99c43c6e8c4f1452036be
Arnau Garcia Marti <arnaugarciaimarti@gmail.com>
2024-10-01 12:40:09 +0200

    fully working Earth

--- src/components/EarthBackground.jsx
+++ src/components/EarthBackground.jsx
@@ -4,14 +4,14 @@ import * as THREE from 'three';
 
 export default function EarthBackground() {
     const earthRef = useRef(null);
-    const texture = useLoader(THREE.TextureLoader, '/assets/3d/texture_earth.jpg');
+    const texture = useLoader(THREE.TextureLoader, '/assets/textures/earthmap1k.jpg');
     const scale = 1.7; //Set size of Earth
 
     // Use the useFrame hook to rotate the Earth
     useFrame(() => {
         if (earthRef.current) {
-            earthRef.current.rotation.y += 0.0015; // Adjust the speed of rotation here
-            earthRef.current.rotation.x += 0.0015;
+            earthRef.current.rotation.y += 0.003;
+            earthRef.current.rotation.y += 0.0001;
         }
     });
 
