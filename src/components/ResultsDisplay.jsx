0840e666e0570f43e7539a0b136a4a67e86a855b
Shiyi <scheng1@upv.edu.es>
2024-10-06 01:14:56 +0200

    Nuevo grid de descargas

--- src/components/ResultsDisplay.jsx
+++ src/components/ResultsDisplay.jsx
@@ -5,6 +5,11 @@ import 'jspdf-autotable';
 
 export default function ResultsDisplay() {
     const [token, setToken] = useState('');
+    const [results, setResults] = useState(null);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [showResults, setShowResults] = useState(false);
+
     const dataUrl = "http://127.0.0.1:5000/api/get-historical-data";
 
     const fetchData = async () => {
@@ -32,24 +37,13 @@ export default function ResultsDisplay() {
         }
     };
 
-    const [results, setResults] = useState({
-        images: Array(9).fill(null).map((_, i) => ({
-            id: i + 1,
-            title: `Landsat Image ${i + 1}`,
-            description: 'Default image. Search to see actual Landsat data.',
-            datos: {}
-        }))
-    });
-
-    const [selectedImage, setSelectedImage] = useState(results.images[0]);
-    const [loading, setLoading] = useState(false);
-
     const handleSearch = async () => {
         setLoading(true);
         try {
             const data = await fetchData();
             setResults(data);
             setSelectedImage(data.images[0]);
+            setShowResults(true); // Mostrar los resultados tras la búsqueda
         } catch (error) {
             console.error('Error fetching data:', error);
         } finally {
@@ -60,7 +54,6 @@ export default function ResultsDisplay() {
     const generatePDF = async (results) => {
         const doc = new jsPDF();
 
-        // Title centered on the first page
         doc.setFontSize(22);
         doc.text("Landsat", 105, 15, { align: "center" });
 
@@ -84,58 +77,41 @@ export default function ResultsDisplay() {
         for (const [index, image] of results.images.entries()) {
             if (index > 0) doc.addPage();
 
-            try {
-                const imgElement = await loadImage(image.url); // Load the image
-                const imgWidth = imgElement.width; // Get the width
-                const imgHeight = imgElement.height; // Get the height
+            const imgWidth = 170;
+            const imgHeight = 100;
 
-                // Maintain the aspect ratio and scale the image
-                const maxWidth = 170; // Maximum width
-                const maxHeight = 100; // Maximum height
-                let scaledWidth = imgWidth;
-                let scaledHeight = imgHeight;
+            const imgRatio = image.width / image.height;
+            let scaledWidth = imgWidth;
+            let scaledHeight = imgHeight;
 
-                const imgRatio = imgWidth / imgHeight;
-
-                if (imgRatio > 1) {
-                    // Image is wider than it is tall
-                    scaledWidth = maxWidth;
-                    scaledHeight = maxWidth / imgRatio;
-                } else {
-                    // Image is taller than it is wide
-                    scaledHeight = maxHeight;
-                    scaledWidth = maxHeight * imgRatio;
-                }
-
-                doc.addImage(imgElement, 'PNG', 20, 30, scaledWidth, scaledHeight);
-
-                // Add the description
-                doc.setFontSize(14); // Font for the title of the description
-                doc.text("Descripciones:", 20, 140);
-
-                doc.setFontSize(10); // Smaller font for the description
-                let yPosition = 150;
-                Object.entries(image.datos).forEach(([key, value], i) => {
-                    doc.text(`${i + 1}. ${key}: ${value}`, 30, yPosition);
-                    yPosition += 8; // Spacing adjusted for smaller text
-                });
-            } catch (error) {
-                console.error('Error loading image:', error);
-                doc.text(`Error loading image: ${image.url}`, 20, 30); // Optional error handling in PDF
+            if (imgRatio > 1) {
+                scaledHeight = imgWidth / imgRatio;
+            } else {
+                scaledWidth = imgHeight * imgRatio;
             }
-        }
+
+            doc.addImage(image.url, 'PNG', 20, 30, scaledWidth, scaledHeight);
+
+            doc.setFontSize(14);
+            doc.text("Descripciones:", 20, 140);
+
+            doc.setFontSize(10);
+            let yPosition = 150;
+            Object.entries(image.datos).forEach(([key, value], i) => {
+                doc.text(`${i + 1}. ${key}: ${value}`, 30, yPosition);
+                yPosition += 8;
+            });
+        });
 
         doc.save("landsat_results.pdf");
     };
-
-
 
     const handleDownloadPDF = () => {
         generatePDF(results);
     };
 
     return (
-        <div className="min-h-screen bg-black bg-opacity-50 rounded-lg py-8 px-4">
+        <div className="bg-black bg-opacity-50 rounded-lg py-8 px-4">
             <div className="max-w-6xl mx-auto space-y-8">
                 <h1 className="text-4xl font-bold text-center text-white">Results</h1>
                 <h3 className="text-3xl font-bold text-center text-white flex items-center justify-center">
@@ -161,55 +137,54 @@ export default function ResultsDisplay() {
                     </button>
                 </div>
 
-                <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-                    <div className="lg:col-span-2 grid grid-cols-3 gap-4">
-                        {results.images.map((image) => (
-                            <div
-                                key={image.id}
-                                className="relative cursor-pointer overflow-hidden rounded-lg aspect-square group"
-                                onClick={() => setSelectedImage(image)}
-                            >
-                                <img
-                                    src={image.url}
-                                    alt={image.title}
-                                    className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
-                                />
-                                <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
-                                    <Satellite className="text-white w-12 h-12" />
-                                </div>
-                                <p className="absolute bottom-0 left-0 right-0 text-white text-sm p-2 truncate bg-black bg-opacity-50">{image.title}</p>
+                {/* Mostrar solo si se han obtenido resultados */}
+                {showResults && (
+                    <>
+                        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+                            <div className="lg:col-span-2 grid grid-cols-3 gap-4">
+                                {results.images.map((image) => (
+                                    <div
+                                        key={image.id}
+                                        className="relative cursor-pointer overflow-hidden rounded-lg aspect-square group"
+                                        onClick={() => setSelectedImage(image)}
+                                    >
+                                        <img
+                                            src={image.url}
+                                            alt={image.title}
+                                            className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
+                                        />
+                                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
+                                            <Satellite className="text-white w-12 h-12" />
+                                        </div>
+                                        <p className="absolute bottom-0 left-0 right-0 text-white text-sm p-2 truncate bg-black bg-opacity-50">{image.title}</p>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
 
-                    <div className="bg-gray-800 rounded-lg p-4 space-y-4">
-                        <img
-                            src={selectedImage.url}
-                            alt={selectedImage.title}
-                            className="w-auto h-auto max-h-96 object-contain rounded-lg"
-                        />
-                        <h4 className="text-xl font-semibold flex items-center space-x-2 text-blue-300">
-                            <Info className="w-5 h-5" />
-                            <span>{selectedImage.title}</span>
-                        </h4>
-                        <p className="text-sm text-white">{selectedImage.description}</p>
-                        <div className="space-y-2 text-white">
-                            {Object.entries(selectedImage.datos).map(([key, value]) => (
-                                <p key={key} className="text-sm"><strong>{key}:</strong> {value}</p>
-                            ))}
+                            <div className="bg-gray-800 rounded-lg p-4 space-y-4 max-h-[750px] overflow-y-auto">
+                                <img
+                                    src={selectedImage.url}
+                                    alt={selectedImage.title}
+                                    className="w-auto h-auto max-h-96 object-contain rounded-lg"
+                                />
+                                <h4 className="text-xl font-semibold flex items-center space-x-2 text-blue-300">
+                                    <Info className="w-5 h-5" />
+                                    <span>{selectedImage.title}</span>
+                                </h4>
+                                <p className="text-sm text-white">{selectedImage.description}</p>
+
+                                {/* Make this section scrollable */}
+                                <div className="space-y-2 text-white">
+                                    {Object.entries(selectedImage.datos).map(([key, value]) => (
+                                        <p key={key} className="text-sm">
+                                            <strong>{key}:</strong> {value}
+                                        </p>
+                                    ))}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-
-                <div className="flex justify-center">
-                    <button
-                        onClick={handleDownloadPDF}
-                        className="bg-green-600 hover:bg-green-700 text-white font-bold py-3 px-6 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition duration-300 flex items-center"
-                    >
-                        <Download className="mr-2 h-5 w-5" />
-                        Download Results as PDF
-                    </button>
-                </div>
+                    </>
+                )}
             </div>
         </div>
     );
